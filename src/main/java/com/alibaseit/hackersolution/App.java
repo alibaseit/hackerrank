f2efd1ea6437767840356d492623333caacfeb94
alibaseit <alibase@gmail.com>
2017-01-29 17:14:16 +0300

    maven project created

---
+++ src/main/java/com/alibaseit/hackersolution/App.java
@@ -0,0 +1,13 @@
+package com.alibaseit.hackersolution;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
