e20c0caf74b392f4b7c942457ae21334213806e7
alibaseit <alibase@gmail.com>
2017-01-29 18:02:00 +0300

    bigdecimal solution added

---
+++ src/main/java/com/alibaseit/hackersolution/java/bignumber/bigdecimal/Solution.java
@@ -0,0 +1,32 @@
+package com.alibaseit.hackersolution.java.bignumber.bigdecimal;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		// Input
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String[] s = new String[n + 2];
+		for (int i = 0; i < n; i++) {
+			s[i] = sc.next();
+		}
+		sc.close();
+		Arrays.sort(s, 0, n, Collections.reverseOrder(new Comparator<String>() {
+			@Override
+			public int compare(String a1, String a2) {
+				BigDecimal a = new BigDecimal(a1);
+				BigDecimal b = new BigDecimal(a2);
+				return a.compareTo(b);
+			}
+		}));
+
+		for (int i = 0; i < n; i++) {
+			System.out.println(s[i]);
+		}
+	}
+}
