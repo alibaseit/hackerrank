cee1b8dc1d267080dfaa9b158aa1f7df15fbb762
alibaseit <alibase@gmail.com>
2017-01-29 18:09:56 +0300

    bigdecimal solution added

---
+++ src/main/java/com/alibaseit/hackersolution/java/bignumber/biginteger/Solution.java
@@ -0,0 +1,15 @@
+package com.alibaseit.hackersolution.java.bignumber.biginteger;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		BigInteger a = new BigInteger(in.nextLine());
+		BigInteger b = new BigInteger(in.nextLine());
+		in.close();
+		System.out.println(a.add(b).toString());
+		System.out.println(a.multiply(b).toString());
+	}
+}
