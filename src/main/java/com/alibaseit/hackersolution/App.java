6f3e60740868a5f71cbb0c60ea7e603e99c6a049
alibaseit <alibase@gmail.com>
2017-01-29 16:25:00 +0200

    second commit

--- src/main/java/com/alibaseit/hackersolution/App.java
+++ src/main/java/com/alibaseit/hackersolution/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hacker Rank Solutions are gathered" );
+        System.out.println( "Hacker Rank Solutions are gathereda" );
     }
 }
